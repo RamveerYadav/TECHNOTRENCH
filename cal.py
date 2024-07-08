5ff2925e0a6f0a5bf47e1356a7a7fb782de72018
RamveerYadav <yramveer259@gmail.com>
2024-07-08 16:28:26 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,19 @@
+# Calculator  Program
+
+num1 = float(input("Enter the Number1:-"))
+num2 = float(input("Enter the Number2:-"))
+opr = input("Enter the Operator:-")
+if opr == "+":
+    print(num1+num2)
+elif opr == "*":
+    print(num1*num2)
+elif opr == "-":
+    print(num1-num2)  
+elif opr == "/":
+    print(num1/num2) 
+else:
+    print("Invalid")   
+    
+
+
+    
